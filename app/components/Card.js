4284c4c45665f01433c7a1ab610aaf8e1e193dfb
Akshar Yeccherla <aksharye@gmail.com>
2024-06-15 10:31:36 -0400

    change card style for mobile, load images

--- app/components/Card.js
+++ app/components/Card.js
@@ -3,12 +3,13 @@
 import { FaGithub } from 'react-icons/fa'
 
 let iconStyle = {'color': '#bae6fd'}
+const URI = "https://personal-website-4w9i.vercel.app/"
 
 const Card = (props) => {
     return (
         <div class="group">
-        <div class="overflow-hidden transition duration-1000 group-hover:scale-105 flex rounded-3xl w-[40vmin] h-[56vmin] object-cover object-center group-hover:opacity-100 opacity-50">
-          <div class="absolute top-[28vmin] rounded-b-3xl p-5 opacity-80 bg-gray-950 w-[40vmin] h-[28vmin] object-cover object-center">
+        <div class="overflow-hidden transition duration-1000 group-hover:scale-105 flex rounded-3xl w-[80vmin] md:w-[40vmin] h-[112vmin] md:h-[56vmin] object-cover object-center group-hover:opacity-100 opacity-50">
+          <div class="absolute top-[56vmin] md:top-[28vmin] rounded-b-3xl p-5 opacity-80 bg-gray-950 w-[80vmin] md:w-[40vmin] h-[56vmin] md:h-[28vmin] object-cover object-center">
             <div class="absolute group-hover:cursor-pointer text-4xl bottom-[2vmin] right-[2vmin] " onClick={() => window.open(props.github, '_blank')}><FaGithub style={iconStyle}/></div>
             <h2 class="text-sky-100 flex justify-center font-bold text-xl">{props.title}</h2>
             <div class="mt-5 text-md text-sky-100 text-center">
@@ -20,7 +21,7 @@ const Card = (props) => {
               </div>
             </div>
           </div>
-          <img class="image w-[40vmin] rounded-3xl h-[56vmin] object-cover object-center" src={props.image} draggable="false" />
+          <img class="image rounded-3xl w-[80vmin] md:w-[40vmin] h-[112vmin] md:h-[56vmin] object-cover object-center" src={URI + props.image} draggable="false" />
 
         </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -6,13 +6,13 @@ import { useEffect } from "react";
 import { FaGithub, FaLinkedin, FaFile} from 'react-icons/fa'
 
 const data = [
-  {title: "CNCM Online", image: "http://localhost:3000/water.jpg", github: "https://github.com/central-nc-math-group/cncm-online", tags: ["Next.js", "MongoDB", "Firebase"], desc: "An innovative contest platform with real-time scoring and leaderboard updates. Inspired by the Codeforces model, but for math conmpetitions."},
-  {title: "Delaunay Mesh Deformation", image: "http://localhost:3000/vizag.jpg", github: "https://github.com/aksharye/delaunay-mesh-deformation", tags: ["Python", "Algorithms"], desc: "Python implementations of several Delaunay-based mesh deformation algorithm, such as DGM and DGM-RBF, with novel improvements."},
-  {title: "Conditional Model Reliance", image: "http://localhost:3000/dubai.jpg", github: "https://github.com/aksharye/conditional-model-reliance", tags: ["Python", "Machine Learning"], desc: "A Python package for computing the conditional model reliance (CMR) variable importance metric, as outlined in (paper)."},
-  {title: "Wyvern", image: "http://localhost:3000/fall.jpg", github: "https://github.com/aksharye/wyvern-frontend", tags: ["Svelte", "Peer.js", "Socket.IO"], desc: "Lightweight video-call platform with viewer-level scene control. Think OBS, but in the hands of the viewer."},
-  {title: "Variable Importance Tester", image: "http://localhost:3000/sven.jpg", github: "https://github.com/KevinH2003/variable-importance", tags: ["Python", "ML"], desc: "Python package to test datasets against combinations of feature screening, models, and variable importance metrics."},
-  {title: "This Website", image: "http://localhost:3000/pandas.jpg", github: "https://github.com/aksharye/personal-website", tags: ["Next.js", "Tailwind"], desc: "Source code for this website to house my cool projects and photography! Parallax effect inspired from (website)."},
-  {title: "Mask R-CNN for Cars", image: "http://localhost:3000/bird.jpg", github: "https://github.com/aksharye/mask-rcnn-car-detection", tags: ["Python", "Machine Learning"], desc: "Object detection model for detecting cars on dashcam-view images, developed with Mask-RCNN, TensorFlow, and Keras."}
+  {title: "CNCM Online", image: "water.jpg", github: "https://github.com/central-nc-math-group/cncm-online", tags: ["Next.js", "MongoDB", "Firebase"], desc: "An innovative contest platform with real-time scoring and leaderboard updates. Inspired by the Codeforces model, but for math conmpetitions."},
+  {title: "Delaunay Mesh Deformation", image: "vizag.jpg", github: "https://github.com/aksharye/delaunay-mesh-deformation", tags: ["Python", "Algorithms"], desc: "Python implementations of several Delaunay-based mesh deformation algorithm, such as DGM and DGM-RBF, with novel improvements."},
+  {title: "Conditional Model Reliance", image: "dubai.jpg", github: "https://github.com/aksharye/conditional-model-reliance", tags: ["Python", "Machine Learning"], desc: "A Python package for computing the conditional model reliance (CMR) variable importance metric, as outlined in (paper)."},
+  {title: "Wyvern", image: "fall.jpg", github: "https://github.com/aksharye/wyvern-frontend", tags: ["Svelte", "Peer.js", "Socket.IO"], desc: "Lightweight video-call platform with viewer-level scene control. Think OBS, but in the hands of the viewer."},
+  {title: "Variable Importance Tester", image: "sven.jpg", github: "https://github.com/KevinH2003/variable-importance", tags: ["Python", "ML"], desc: "Python package to test datasets against combinations of feature screening, models, and variable importance metrics."},
+  {title: "This Website", image: "pandas.jpg", github: "https://github.com/aksharye/personal-website", tags: ["Next.js", "Tailwind"], desc: "Source code for this website to house my cool projects and photography! Parallax effect inspired from (website)."},
+  {title: "Mask R-CNN for Cars", image: "bird.jpg", github: "https://github.com/aksharye/mask-rcnn-car-detection", tags: ["Python", "Machine Learning"], desc: "Object detection model for detecting cars on dashcam-view images, developed with Mask-RCNN, TensorFlow, and Keras."}
 ]
 
 export default function Home() {
@@ -100,28 +100,28 @@ export default function Home() {
     window.ontouchmove = e => handleOnMove(e.touches[0]);
   })
 
-  let iconStyle = {'color': 'white', 'margin': "12px"}
+  let iconStyle = {'color': '#e0f2fe', 'margin': "6px"}
 
   return (
     <div className="m-0 overscroll-none min-h-screen bg-stone-950">
       <nav class="flex min-w-screen">
-        <div class="text-xl w-1/2 px-6 py-3 flex">
-          <a href="#" class="px-4 py-2 text-sky-100 hover:px-4 hover:py-2 hover:rounded-md hover:bg-opacity-25 hover:bg-slate-200 hover:text-gray-200">Home</a>
-          <a href="#" class="px-4 py-2 text-sky-100 hover:px-4 hover:py-2 hover:rounded-md hover:bg-opacity-25 hover:bg-slate-200 hover:text-gray-200">About</a>
-          <a href="#" class="px-4 py-2 text-sky-100 hover:px-4 hover:py-2 hover:rounded-md hover:bg-opacity-25 hover:bg-slate-200 hover:text-gray-200">CV</a>
+        <div class="text-xl w-1/2 px-2 md:px-6 py-3 flex">
+          <a href="#" class="px-2 md:px-4 py-2 text-sky-100 hover:px-4 hover:py-2 hover:rounded-md hover:bg-opacity-25 hover:bg-slate-200 hover:text-gray-200">Home</a>
+          <a href="#" class="px-2 md:px-4 py-2 text-sky-100 hover:px-4 hover:py-2 hover:rounded-md hover:bg-opacity-25 hover:bg-slate-200 hover:text-gray-200">About</a>
+          <a href="#" class="px-2 md:px-4 py-2 text-sky-100 hover:px-4 hover:py-2 hover:rounded-md hover:bg-opacity-25 hover:bg-slate-200 hover:text-gray-200">CV</a>
         </div>
-        <div class="w-1/2 flex text-4xl items-center justify-end px-6 mx-auto">
+        <div class="w-1/2 flex text-2xl md:text-4xl items-center justify-end px-2 md:px-6 mx-auto">
             <FaGithub style={iconStyle}/>
             <FaLinkedin style={iconStyle}/>
             <FaFile style={iconStyle}/>
           </div>
       </nav>
 
-      <div class="text-sky-100 w-screen block justify-center h-[30vh] items-center">
-          <div class="flex pt-[5vh] justify-center items-center text-7xl font-extrabold">Akshar Yeccherla</div>
+      <div class="text-sky-100 w-screen block justify-center h-[15vh] md:h-[30vh] items-center">
+          <div class="flex pt-[5vh] justify-center items-center text-4xl md:text-7xl font-extrabold">Akshar Yeccherla</div>
 
 
-          <div class="flex h-1/4 text-3xl justify-center pt-[2vh]">
+          <div class="flex h-1/4 text-2xl md:text-3xl justify-center pt-[2vh]">
             Super GM
           </div>
 
@@ -133,7 +133,7 @@ export default function Home() {
       
       
 
-      <div id="image-track" class="flex overscroll-none gap-[4vmin] absolute left-[50%] top-[65%] translate-x-[-50%] translate-y-[-50%] select-none" data-mouse-down-at="0" data-prev-percentage="0">
+      <div id="image-track" class="flex overscroll-none gap-[4vmin] absolute left-[50%] top-[60%] md:top-[65%] translate-x-[-50%] translate-y-[-50%] select-none" data-mouse-down-at="0" data-prev-percentage="0">
           {data.map((entry) => {
             return <Card key={entry.title} title={entry.title} desc={entry.desc} github={entry.github} tags={entry.tags} image={entry.image}/>
           })}
